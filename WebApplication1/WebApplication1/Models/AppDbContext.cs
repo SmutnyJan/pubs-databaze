34a9fd09f74bf7979dbce30ab9c7047b2974ac6a
SmutnyJan <jan.smutny@pslib.cz>
2020-09-25 11:53:00 +0200

    initial commit

---
+++ WebApplication1/WebApplication1/Models/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+        public DbSet<Pub> Pubs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Pub>().HasData(
+                new Pub { Id = 1, Name = "Svah", Municipality = "Liberec"},
+                new Pub { Id = 2, Name = "Techtle", Municipality = "Jablonec nad Nisou" }
+            );
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using WebApplication1.Models;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly AppDbContext _db;
+        public List<Pub> Pubs { get; set; }
+        public IndexModel(ILogger<IndexModel> logger, AppDbContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+            Pubs = _db.Pubs.ToList();
+            
+        }
+    }
+    //_db.Pubs(new Pub {Name = "", Municipality = ""});
+    //_db.SaveChanges();
+}
